6a9694d4b6ad42021fe58624f8494bdd15289404
maaqoul <hicham.maaqoul@gmail.com>
2018-10-16 11:13:05 +0100

    adds delete function

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ let bookMarkIt = () => {
     const siteName = document.getElementById('siteName').value;
     const siteUrl = document.getElementById('siteUrl').value;
 
+    if(!validateForm(siteName, siteUrl)) {
+        return false;
+    }
+
     let bookmarkObject = {
         name: siteName,
         url: siteUrl
@@ -16,15 +20,20 @@ let bookMarkIt = () => {
         bookmarkArray.push(bookmarkObject);
         localStorage.setItem('bookmarks', JSON.stringify(bookmarkArray));
     }
+    document.getElementById('myForm').reset();
+    fetchBookMarks();
 }
 
-function deleteBookMark(url) {
+let deleteBookMark = (url) => {
     let bookmarkArray = JSON.parse(localStorage.getItem('bookmarks'));
-    for(const bookmark of bookmarkArray) {
-        if (bookmark.url === url ) {
-            localStorage.removeItem('bookmarks');
+    
+    for (let i = 0; i <bookmarkArray.length; ++i) {
+        if (bookmarkArray[i].url === url) {
+            bookmarkArray.splice(i, 1);
         }
     }
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarkArray));
+    fetchBookMarks();
 }
 
 let fetchBookMarks = () => {
@@ -47,3 +56,18 @@ let fetchBookMarks = () => {
     }
 }
 
+let validateForm = (siteName, siteUrl) => {
+    var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+    var regex = new RegExp(expression);
+
+    if (!siteUrl.match(regex)) {
+        alert('please enter a valid url');
+        return false;
+    }
+
+    if (!siteName || !siteUrl) {
+        alert('enter a name or url');
+        return false;
+    }
+    return true;
+}
